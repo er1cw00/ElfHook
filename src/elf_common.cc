714fc0429e5229fb8914221b4b603c58c9e23376
anahadaw <wadahana@qq.com>
2018-04-30 16:09:34 +0800

    add dump_hex function

--- src/elf_common.cc
+++ src/elf_common.cc
@@ -12,3 +12,21 @@ bool safe_add(off64_t* out, off64_t a, size_t b) {
     *out = a + b;
     return true;
 }
+
+void dump_hex(uint8_t * pbuf, int size) {
+        int i = 0;
+        for (int j = 0; j < size; j += 16) {
+            i = j;
+            fprintf(stderr, "%02X %02X %02X %02X %02X %02X %02X %02X  ", 
+                pbuf[i + 0], pbuf[i + 1], pbuf[i + 2], pbuf[i + 3],
+                pbuf[i + 4], pbuf[i + 5], pbuf[i + 6], pbuf[i + 7]);
+            fprintf(stderr, "%02X %02X %02X %02X %02X %02X %02X %02X\n", 
+                pbuf[i + 8], pbuf[i + 9], pbuf[i + 10], pbuf[i + 11],
+                pbuf[i + 12], pbuf[i + 13], pbuf[i + 14], pbuf[i + 15]);
+        }
+        for (int j = i; j < size; j += 1) {
+            fprintf(stderr, "%02X ", pbuf[j]);
+        }
+        fprintf(stderr, "\n");
+        return;
+    }
